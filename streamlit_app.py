d0b23eaf7c80d86cd2a23966c98caa7d39ddfd31
Sinchen-Gundmi <62190832+Sinchen-Gundmi@users.noreply.github.com>
2023-10-28 10:59:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-streamlit.title('What's for Breakfast?')
+streamlit.title('Whats for Breakfast?')
 streamlit.header('Breakfast Menu') 
 streamlit.text('Omega 3 & Blueberry Oatmeal') 
 streamlit.text('Kale, Spinach & Rocket Smoothie')
